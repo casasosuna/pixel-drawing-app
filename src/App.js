2292cc0417acde42b4e01c0f5d89820f06aa1caf
Enrique Casas <enriquecasas@Enriques-MacBook-Pro.local>
2022-01-05 15:31:30 -0600

    let user choose a color

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Pixel from './components/Pixel'
@@ -6,9 +7,12 @@ import Canvas from "./components/Canvas"
 import ColorPicker from "./components/ColorPicker"
 
 function App() {
+
+  const [color, setColor] = useState(0)
+  
   return (
     <div className="App">
-      <ColorPicker />
+      <ColorPicker currentColor={color} setColor={color => setColor(color)}/>
       <Canvas />
     </div>
   );
--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -6,7 +6,12 @@ export default props => {
     return (
         <div className="colorpicker">
             {Colors.map((color, index) => {
-                return <Pixel key={index} background={color}/>
+                return <Pixel
+                    onClick={e => props.setColor(index)} 
+                    key={index} 
+                    background={color}
+                    current={Colors[props.currentColor] === color}
+                    />
             })}
         </div>
     )
