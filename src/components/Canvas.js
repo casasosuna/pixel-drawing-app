6fad29a6b27ce34038ee047eb11fbb8ad0e90f17
Enrique Casas <enriquecasas@Enriques-MacBook-Pro.local>
2022-01-07 14:35:27 -0600

    Store drawing in local storage

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,16 +1,36 @@
-import React, { Component, useState } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import Colors from '../components/Colors'
 import Pixel from '../components/Pixel'
 
 const Canvas = props => {
+
     const [matrix, setMatrix] = useState(Array(30)
         .fill()
         .map(() => Array(30).fill(0)))
+    
+    useEffect(
+        () => {
+            if (JSON.parse(localStorage.getItem("new_canvas"))) {
+                setMatrix(JSON.parse(localStorage.getItem("new_canvas")))
+            } 
+        }, []
+    )
+
+
+    
+    /* 
+    const localArray = JSON.parse(localStorage.getItem("new_canvas"))
+    setMatrix(localArray)
+    console.log("here") */
 
     const changeColor = (rowIndex, colIndex) => {
         const newMatrix = JSON.parse(JSON.stringify(matrix))
         newMatrix[rowIndex][colIndex] = props.currentColor
         setMatrix(newMatrix)
+        const jsonMatrix = JSON.stringify(newMatrix)
+        localStorage.setItem("new_canvas", jsonMatrix)
+        console.log(typeof localStorage.getItem("new_canvas"))
+        console.log(localStorage.getItem("new_canvas"))
     }
 
     return (
